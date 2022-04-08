7aa82ac1341ca46abd9055f7c588820b23591511
Nicholas Zwiren <zwnick68@gmail.com>
2022-04-08 13:25:56 -0400

    added useEffect with paragraphs

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,22 +1,33 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect,useState,React } from "react"
+
 const Timeline = () => {
     const [posts, setPosts] =useState([])
-    useEffect(async() =>{
-        console.log("rendering timeline...")
+    useEffect(() =>{
+        // console.log("rendering timeline...")
+        (async() =>{
+        
         let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
         let res = await req.json()
         setPosts(res)
-        
+        })()
     }, [])
     
     return(
         <div>
             <h3>Timeline</h3>
             {
-
+                posts.map((element)=> {
+                    return(
+                        <div>
+                    <p>{element.username}</p>
+                    <p>{element.content}</p>
+                    </div>
+                    )
+                })
+    
             }
         </div>
     )
 }
+
 export default Timeline
