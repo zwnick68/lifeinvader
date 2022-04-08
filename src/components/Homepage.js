201a017fec61bd4312922f2213a4ace318198c65
Nicholas Zwiren <zwnick68@gmail.com>
2022-04-08 14:40:47 -0400

    added css

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
 
 import Timeline from "./Timeline.js"
 import Sidebar from "./Sidebar.js"
+import NavBar from "./NavBar.js";
 const Homepage = () => {
     return (
         <div>
+          <NavBar/>
           <Timeline/>
           <Sidebar/>
         </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return(
+        <nav>
+        <div>
+            <a href="#" className="home-link">LifeInvader</a>
+        </div>
+        <div>
+            <i class="fa-solid fa-magnfying-glass"></i>
+            <input type="search" placeholder="search"></input> 
+        </div>
+        <div>
+            <i class= "fa solid fa-bell"></i>
+        </div>
+        </nav>
+    )
+}
+export default NavBar
